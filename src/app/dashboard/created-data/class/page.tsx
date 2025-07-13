d57a552d758bbe2a48f3bd8e9afe0f7a59625ba1
Robiu-Sani <hafazrobiussani@gmail.com>
2025-07-13 17:01:10 +0600

    footer is done

--- src/app/dashboard/created-data/class/page.tsx
+++ src/app/dashboard/created-data/class/page.tsx
@@ -1,12 +1,8 @@
 import React from "react";
-import AllClass from "./AllClass";
-import axios from "axios";
+// import AllClass from "./AllClass";
+// import axios from "axios";
 
 export default async function page() {
-  const result = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_V1}/branch`);
-  return (
-    <div>
-      <AllClass data={result.data.data.classes} />
-    </div>
-  );
+  // const result = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_V1}/branch`);
+  return <div>{/* <AllClass data={result.data.data.classes} /> */}</div>;
 }
